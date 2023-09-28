4ddf1df5668f151568b85010e64a108b55454d40
Ismael <imarram1104@g.educaand.es>
2023-09-28 12:52:10 +0200

    Ej1 Bien, Ej2 Bien

--- src/main/java/org/iesvdm/haversine/Extensiones.java
+++ src/main/java/org/iesvdm/haversine/Extensiones.java
@@ -1,4 +1,31 @@
 package org.iesvdm.haversine;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Extensiones {
+    public final BigDecimal radioTierraKM= new BigDecimal(6378.0);
+    public static BigDecimal distanciaKM  (posicion posicionOrigen, posicion posicionDestino){
+        BigDecimal difLatitud = (posicionOrigen.getLatitud().subtract(posicionDestino.getLatitud()));
+        BigDecimal difLongitud = (posicionOrigen.getLongitud().subtract(posicionDestino.getLongitud()));
+        BigDecimal difLatitudRad = enRadianes(difLatitud);
+        BigDecimal difLongitudRad = enRadianes(difLongitud);
+        BigDecimal dos = new BigDecimal(2);
+        var primeraLinea = BigDecimal.valueOf(Math.sin(difLatitudRad.divide(BigDecimal.TWO, 12, RoundingMode.HALF_UP).pow(2).doubleValue()));
+        var segundaLinea = BigDecimal.valueOf(Math.cos(enRadianes(posicionOrigen.getLatitud()).doubleValue()));
+        var terceraLinea = BigDecimal.valueOf(Math.cos(enRadianes(posicionDestino.getLatitud()).doubleValue()));
+        var cuartaLinea = BigDecimal.valueOf(Math.sin(difLongitudRad.divide(BigDecimal.TWO, 12, RoundingMode.HALF_UP).pow(2).doubleValue()));
+
+        var a = primeraLinea.add(segundaLinea.multiply(terceraLinea.multiply(cuartaLinea)));
+        BigDecimal unomenosa = BigDecimal.ONE.subtract(a);
+        double raiz = unomenosa.sqrt(M)
+        return null;
+    }
+    public static BigDecimal enRadianes(BigDecimal valor){
+        BigDecimal piBD = new BigDecimal(Math.PI);
+        BigDecimal cientochenta = new BigDecimal(180);
+        BigDecimal valorRadianes = (valor.multiply(piBD)).divide(cientochenta, RoundingMode.DOWN);
+        return  valorRadianes;
+
+    }
 }
