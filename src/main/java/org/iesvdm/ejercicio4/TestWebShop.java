f044299392e1db10de01d3f5538f6a2bb01607b5
Ismael <imarram1104@g.educaand.es>
2023-09-29 14:33:46 +0200

    Ej4

--- src/main/java/org/iesvdm/ejercicio4/TestWebShop.java
+++ src/main/java/org/iesvdm/ejercicio4/TestWebShop.java
@@ -2,8 +2,11 @@ package org.iesvdm.ejercicio4;
 
 import java.time.LocalDate;
 
-public class Test {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class TestWebShop {
+@Test
     public void testWebAccount() throws Exception{
         Address address1 = new Address("Gran Via", 1, "Madrid", "España");
         Phone phone1 = new Phone("657889900", "987677990");
@@ -12,6 +15,12 @@ public class Test {
         Account account = new Account("1", address1, false, LocalDate.now());
         customer1.setAccount(account);
         customer1.setWebUser(webUser);
-    }
+        Customer customer2 = new Customer("2", new Address("Goya", 12, "Madrid","España"),new Phone("666666666", "999999999"), "paco22@gmail.com");
+        WebUser webuser2 = new WebUser("2", "4321");
+        Account account2 = new Account("2", new Address("Goya", 12, "Madrid", "España"),false, LocalDate.now() );
+        customer2.setAccount(account2);
+        customer2.setWebUser(webuser2);
+    Assertions.assertEquals("");
+}
 
 }
